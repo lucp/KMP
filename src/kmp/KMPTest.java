24c92d6a5666b957eec132fc10a0d9f3c0786b54
lucp <l.potepa@gmail.com>
2015-01-29 21:49:55 +0100

    Tests

--- src/kmp/KMPTest.java
+++ src/kmp/KMPTest.java
@@ -11,7 +11,42 @@ public class KMPTest {
 		String text = "ABBACABAABCDABDBDBAC";
 		String pattern = "ABCDABD";
 		int match = KMP.search(text, pattern);
-		System.out.println(match);
+		System.out.println(text);
+		for (int i = 0; i < match; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(pattern);
+		System.out.println("Position: " + match);
+		
+		text = "TO BE OR NOT TO BE";
+		pattern = "NOT";
+		match = KMP.search(text, pattern);
+		System.out.println(text);
+		for (int i = 0; i < match; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(pattern);
+		System.out.println("Position: " + match);
+		
+		text = "WYSZUKAJ WZORZEC";
+		pattern = "WZOR";
+		match = KMP.search(text, pattern);
+		System.out.println(text);
+		for (int i = 0; i < match; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(pattern);
+		System.out.println("Position: " + match);
+		
+		text = "WYSZUKAJ WZORZEC";
+		pattern = "WZOC";
+		match = KMP.search(text, pattern);
+		System.out.println(text);
+		for (int i = 0; i < match; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(pattern);
+		System.out.println("Position: " + match);
 	}
 
 }
